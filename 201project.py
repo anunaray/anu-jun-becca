25ec826d7dbb12bb6c43b13c4de171c8a587ecd9
Anu Narayan <anunarayan@Anus-MacBook-Pro.local>
2025-11-25 14:16:37 -0500

    defined db functions

--- 201project.py
+++ 201project.py
@@ -1,4 +1,20 @@
 '''Anu, Jun, Becca
 SI 201 Museums Project'''
 
+import requests
+import json
+import sqlite3
+
+def create_met_db():
+    pass
+
+def create_harvard_db():
+    pass
+
+def create_chicago_db():
+    pass
+
+def create_coophew_db():
+    pass
+
 
